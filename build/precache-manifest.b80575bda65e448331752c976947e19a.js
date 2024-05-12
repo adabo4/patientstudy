aa55336532c154dbcba6d7d9f9668c4f81b74337
adabo4 <130857407+adabo4@users.noreply.github.com>
2024-05-12 18:28:31 +0000

    Update build to output generated at 570954a

---
+++ build/precache-manifest.b80575bda65e448331752c976947e19a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51d8128f9d704fb96bcbda7055dc4293",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b81b90bfefee51c3d3d9",
+    "url": "/static/css/main.cd9fe1c6.chunk.css"
+  },
+  {
+    "revision": "ef22d059f320ae153d8f",
+    "url": "/static/js/2.94caf74c.chunk.js"
+  },
+  {
+    "revision": "c22e7b4cb20d4556252c2aa78698d764",
+    "url": "/static/js/2.94caf74c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4447ce66ae7e2a8073cf",
+    "url": "/static/js/3.2cfb034d.chunk.js"
+  },
+  {
+    "revision": "b81b90bfefee51c3d3d9",
+    "url": "/static/js/main.10d8b07f.chunk.js"
+  },
+  {
+    "revision": "481a659f51764982d6ab",
+    "url": "/static/js/runtime-main.86f63fec.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bd07be1ce4c7031e22346cfb8dbaffe3.js"
+  "/precache-manifest.b80575bda65e448331752c976947e19a.js"
 );
 
 self.addEventListener('message', (event) => {
